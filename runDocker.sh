7443b4f84c95dcc5721992fda1d89d666bb4b343
lbeaucourt <lbeaucourt@agaetis.fr>
2018-10-29 13:36:43 +0100

    Fix docker volume path

--- runDocker.sh
+++ runDocker.sh
@@ -2,5 +2,5 @@ xhost +local:docker
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -m 8GB -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v /home/leo/Documents/SandBox/realTime-ObjectDetection:/lab  realtime-objectdetection
+docker run -m 8GB -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/lab  realtime-objectdetection
 xhost -local:docker
